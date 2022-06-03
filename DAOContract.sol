8f7a0d47b00e5144364bc239f6a9b89230593356
upa cyxou <57546893+upacyxou@users.noreply.github.com>
2022-06-03 14:48:35 +0400

    Add solidity file and screenshots of remix and zkrepl

---
+++ DAOContract.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract Purchase {
+    uint public value;
+    uint public confirmationTime;
+
+    address payable public seller;
+    address payable public buyer;
+
+    enum State { Created, Locked, Release, Inactive }
+    // The state variable has a default value of the first member, `State.created`
+    State public state;
+
+    modifier condition(bool condition_) {
+        require(condition_);
+        _;
+    }
+
+    /// Only the buyer can call this function.
+    error OnlyBuyer();
+    /// Only the seller can call this function.
+    error OnlySeller();
+    /// The function cannot be called at the current state.
+    error InvalidState();
+    /// The provided value has to be even.
+    error ValueNotEven();
+    /// The required time not ellapsed or called not by buyer
+    error TimeNotEllapsedOrNotBuyer();
+
+    modifier onlyBuyer() {
+        if (msg.sender != buyer)
+            revert OnlyBuyer();
+        _;
+    }
+
+    modifier onlySeller() {
+        if (msg.sender != seller)
+            revert OnlySeller();
+        _;
+    }
+
+    modifier inState(State state_) {
+        if (state != state_)
+            revert InvalidState();
+        _;
+    }
+
+    modifier isLocked() {
+        if (state != State.Locked)
+            revert InvalidState();
+        _;
+    }
+
+    modifier lockedAndOnlyBuyerOrEllapsedTime() {
+        if (state != State.Locked)
+            revert InvalidState();
+        if (msg.sender == buyer || (block.timestamp - confirmationTime) > 300)
+            _;
+        else
+            revert TimeNotEllapsedOrNotBuyer();
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+
+    // Ensure that `msg.value` is an even number.
+    // Division will truncate if it is an odd number.
+    // Check via multiplication that it wasn't an odd number.
+    constructor() payable {
+        seller = payable(msg.sender);
+        value = msg.value / 2;
+        if ((2 * value) != msg.value)
+            revert ValueNotEven();
+    }
+
+    /// Abort the purchase and reclaim the ether.
+    /// Can only be called by the seller before
+    /// the contract is locked.
+    function abort()
+        external
+        onlySeller
+        inState(State.Created)
+    {
+        emit Aborted();
+        state = State.Inactive;
+        // We use transfer here directly. It is
+        // reentrancy-safe, because it is the
+        // last call in this function and we
+        // already changed the state.
+        seller.transfer(address(this).balance);
+    }
+
+    /// Confirm the purchase as buyer.
+    /// Transaction has to include `2 * value` ether.
+    /// The ether will be locked until confirmReceived
+    /// is called.
+    function confirmPurchase()
+        external
+        inState(State.Created)
+        condition(msg.value == (2 * value))
+        payable
+    {
+        emit PurchaseConfirmed();
+        buyer = payable(msg.sender);
+        state = State.Locked;
+        confirmationTime = block.timestamp;
+    }
+
+    function completePurchase()
+        external
+        lockedAndOnlyBuyerOrEllapsedTime
+    {
+        buyer.transfer(value);
+        seller.transfer(3 * value);
+
+        state = State.Inactive;
+
+        emit ItemReceived();
+        emit SellerRefunded();
+    }
+}
